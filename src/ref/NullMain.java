d0d3c5b556abc451047c2432c6551fc85778937a
Park Hyewon <db1648@naver.com>
2024-06-29 14:35:00 +0900

    240629 NullPointerException

---
+++ src/ref/NullMain.java
@@ -0,0 +1,9 @@
+package ref;
+
+public class NullMain {
+    public static void main(String[] args) {
+        InitData data = null;
+        data.value1 = 10;
+        System.out.println(data.value1);
+    }
+}
