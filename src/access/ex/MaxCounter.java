42a4e6f0eb8a2a2188019898247186e1b651f0de
Park Hyewon <db1648@naver.com>
2024-07-04 00:11:32 +0900

    240704 접근제어자 MaxCount / ShoppingCart 예제 문제 풀이

---
+++ src/access/ex/MaxCounter.java
@@ -0,0 +1,19 @@
+package access.ex;
+
+public class MaxCounter {
+    private int count = 0;
+    private int max;
+
+    public MaxCounter(int max) {
+        this.max = max;
+    }
+    public int getCount() {
+        return count;
+    }
+    public void increment() {
+        if (count >= max) {
+            System.out.println("최대값을 초과 할 수 없습니다.");
+            return;
+        } count++;
+    }
+}
