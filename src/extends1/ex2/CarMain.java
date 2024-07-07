920bb6bb459c043d4121bac972fc1a436359d53c
Park Hyewon <db1648@naver.com>
2024-07-07 11:45:21 +0900

    240707 상속 기능과 메모리 구조

---
+++ src/extends1/ex2/CarMain.java
@@ -0,0 +1,22 @@
+package extends1.ex2;
+
+import javax.swing.*;
+
+public class CarMain {
+    public static void main(String[] args) {
+        ElectricCar electricCar = new ElectricCar();
+        electricCar.move();
+        electricCar.openDoor();
+        electricCar.charge();
+
+        GasCar gasCar = new GasCar();
+        gasCar.move();
+        gasCar.openDoor();
+        gasCar.fillUp();
+
+        HydrogenCar hydrogenCar = new HydrogenCar();
+        hydrogenCar.move();
+        hydrogenCar.openDoor();
+        hydrogenCar.fillHydrogen();
+    }
+}
